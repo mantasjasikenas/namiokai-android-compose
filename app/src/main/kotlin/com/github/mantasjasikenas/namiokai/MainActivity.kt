5a7d01f6571e941e1569b54f4b0217f4f138a25a
Mantelis <mantasjasikenas@gmail.com>
2024-01-08 13:00:00 +0200

    * Add EdgeToEdge functionality

--- app/src/main/kotlin/com/github/mantasjasikenas/namiokai/MainActivity.kt
+++ app/src/main/kotlin/com/github/mantasjasikenas/namiokai/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -45,6 +46,7 @@ class MainActivity : ComponentActivity(), AnalyticsLogger by AnalyticsLoggerImpl
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
